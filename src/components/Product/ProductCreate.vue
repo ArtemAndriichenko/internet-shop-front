b69b53164656e320d6eb1008c23c7031977170a4
ArtemAndriichenko <artemandriychenko@gmail.com>
2023-03-13 12:05:07 +0200

    Hot fix

--- src/components/Product/ProductCreate.vue
+++ src/components/Product/ProductCreate.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <form @submit.prevent>
     <div class="create--product">
       <h1 class="create--label">
@@ -67,90 +66,6 @@ export default {
 
 <style scoped>
 .create--product{
-=======
-    <form @submit.prevent>
-      <div class="create--patient">
-        <h1 class="create--label">
-            Create a Product
-        </h1>
-        <div class="input--element">
-          <select class="form-select" v-model="username" @change="setUser()" >
-            <option v-for="item in users" :key="item._id">
-              {{item.username}}
-            </option>
-          </select>
-        </div>
-        <div class="btn--element">
-          <button
-            class="btn btn-success"
-            @click="addProduct();
-            hideDialog()"
-          >Create</button>
-        </div>
-      </div>
-    </form>
-  </template>
-  
-  <script>
-  import axios from "axios";
-  
-  export default {
-    name: 'product-create',
-    data(){
-      return{
-        username: "",
-        userId: "",
-        users: []
-      }
-    },
-    methods: {
-      async addProduct() {
-        try {
-          await axios.post("http://localhost:8081/products", {
-            user_id: this.userId,
-            headers: {
-              Accept: "application/json",
-            }
-          })
-        } catch(error){
-          console.log(error)
-        }
-      },
-      hideDialog(){
-          this.$emit('updateShowAfterCreate', false)
-      },
-      async setUser(){
-        try {
-          const response = await axios.get("http://localhost:8081/users", {
-            headers: {
-              Accept: "application/json",
-            },
-          });
-          const userList = response.data
-          this.userId = userList[userList.findIndex(i => i.username === this.username)].id
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    },
-    async mounted() {
-      try {
-        const response = await axios.get("http://localhost:8081/users", {
-          headers: {
-            Accept: "application/json",
-          },
-        });
-        this.users = response.data
-      } catch (error) {
-        console.error(error);
-      }
-    }
-  }
-  </script>
-  
-<style scoped>
-.create--patient{
->>>>>>> d7f11bc28f458076d4aaad1d9c3de5daab34e20a
     padding-block: 30px;
     padding-inline: 60px;
 }
